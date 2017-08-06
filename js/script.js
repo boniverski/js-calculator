c39a66ae3a6a512938e1c19eda49b770b596fde2
boniverski <bosko.rabrenovic@gmail.com>
2017-08-06 17:58:08 +0200

    update basic logic

--- js/script.js
+++ js/script.js
@@ -8,25 +8,54 @@ $(document).ready(function() {
     if(!isNaN(val) || val === "."){
       tempEntry += val;
       $("#entry").val(tempEntry.substring(0,10));
+      if(tempEntry.includes(".")){
+        $("#point").prop("disabled", true);
+      }
     } //clearing temporary entry value on DEL click
       else if (val === "del"){
+        $("#entry").val("");
         entries = [];
         tempEntry = "";
-        $("#entry").val("");
-    } else if (val === "/" || val === "*" || val === "-" || val === "+"){
-        entries.push(tempEntry);
-        entries.push(val);
+        $("#point").prop("disabled", false);
+        //$("#point").val(".");
+    } else if (val === "/" || val === "*" || val === "+"){
+        if(tempEntry.length > 0) {
+          entries.push(tempEntry);
+          entries.push(val);
+        } else {
+          entries = [];
+        }
         tempEntry = "";
-        console.log(entries);
-    } else if (val === "="){
-        entries.push(tempEntry);
-        tempEntry = "";
-        let arrConvert = entries.join("");
-        let evaluate = eval(arrConvert);
-        $("#entry").val(evaluate);
+    } else if (val === '=') {
+      	entries.push(tempEntry);
 
-        console.log(entries);
-        console.log(evaluate);
-    }
+        //testing in console
+        var consoledEntry = entries.join(" ");
+        console.log(`Input entry: ${consoledEntry}`);
+
+        var nt = Number(entries[0]);
+        for (var i = 1; i < entries.length; i++) {
+          var nextNum = Number(entries[i+1])
+          var symbol = entries[i];
+
+          if (symbol === '+') { nt += nextNum; }
+          else if (symbol === '-') { nt -= nextNum; }
+          else if (symbol === '*') { nt *= nextNum; }
+          else if (symbol === '/') { nt /= nextNum; }
+
+          i++;
+        }
+        // Swap the '-' symbol so text input handles it correctly
+        if (nt < 0) {
+          nt = Math.abs(nt) + '-';
+        }
+
+        $("#entry").val(nt);
+    		entries = [];
+        tempEntry = '';
+
+        //testing in console
+        console.log(`Result: ${nt}`);
+      }
   });
 });
