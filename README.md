b48487a4d8a65dcef484103430d4c4280ac5a463
boniverski <bosko.rabrenovic@gmail.com>
2017-08-07 20:24:26 +0200

    update basic logic 2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#JavaScript Calculator (unfinished)
+# JavaScript Calculator (unfinished)
 
 Simple JS Caclulator, Free Code Camp's advanced project
--- js/script.js
+++ js/script.js
@@ -1,61 +1,59 @@
 $(document).ready(function() {
-  let entries = [];
-  let tempEntry = ""; //Temporary entry value
+  let tempEntry = ""; // Temporary entry value
+  let entries = []; // Final entry before calculation
 
   $("button").on("click", function() {
     let val = $(this).val();
-    //returning numbers in calculator's entry
-    if(!isNaN(val) || val === "."){
+
+    // Returning numbers in calculator's entry
+    if(!isNaN(val) || val === "." || val === "-"){
       tempEntry += val;
       $("#entry").val(tempEntry.substring(0,10));
-      if(tempEntry.includes(".")){
-        $("#point").prop("disabled", true);
-      }
-    } //clearing temporary entry value on DEL click
-      else if (val === "del"){
+     // Clearing temporary entry value on DEL click
+    } else if (val === "clr"){
+        $(".clr").addClass("hidden");
+        $(".del").removeClass("hidden");
         $("#entry").val("");
+        // Clearing all entries
         entries = [];
         tempEntry = "";
-        $("#point").prop("disabled", false);
-        //$("#point").val(".");
+    // Removing last number or sign from entry
+    } else if (val === "del"){
+        $("#entry").val(
+          function(index, value){
+            tempEntry = value.substr(0, value.length - 1);
+            return value.substr(0, value.length - 1);
+          }
+        );
+        $(".clr").addClass("hidden");
+        $(".del").removeClass("hidden");
+    //Pushing math signs
     } else if (val === "/" || val === "*" || val === "+"){
         if(tempEntry.length > 0) {
-          entries.push(tempEntry);
+          entries.push(tempEntry); // Pushing number entered before math sign
           entries.push(val);
         } else {
-          entries = [];
+          entries = ["0", val]; // If math sing is entered before number
         }
         tempEntry = "";
+    // Calculation logic
     } else if (val === '=') {
-      	entries.push(tempEntry);
+        $(".del").addClass("hidden");
+        $(".clr").removeClass("hidden");
+      	entries.push(tempEntry); // Pushing all entries into array
+        let convertedEntry = entries.join(" "); // Than joining all entries into string
+        console.log(`Input entry: ${convertedEntry}`);
 
-        //testing in console
-        var consoledEntry = entries.join(" ");
-        console.log(`Input entry: ${consoledEntry}`);
-
-        var nt = Number(entries[0]);
-        for (var i = 1; i < entries.length; i++) {
-          var nextNum = Number(entries[i+1])
-          var symbol = entries[i];
-
-          if (symbol === '+') { nt += nextNum; }
-          else if (symbol === '-') { nt -= nextNum; }
-          else if (symbol === '*') { nt *= nextNum; }
-          else if (symbol === '/') { nt /= nextNum; }
-
-          i++;
+        // Checking if evaluation has error due to odd entries
+        try {
+          let result = eval(convertedEntry); // Evaluate string with entries
+          $("#entry").val(result);
+          console.log(`Result: ${result}`);
+        } catch (e){
+            if (e instanceof SyntaxError){
+              $("#entry").val("NaN");
+            }
         }
-        // Swap the '-' symbol so text input handles it correctly
-        if (nt < 0) {
-          nt = Math.abs(nt) + '-';
-        }
-
-        $("#entry").val(nt);
-    		entries = [];
-        tempEntry = '';
-
-        //testing in console
-        console.log(`Result: ${nt}`);
       }
   });
 });
