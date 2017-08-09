75119ec952356d8b986a2927db7f8dcc1da9ebe6
boniverski <bosko.rabrenovic@gmail.com>
2017-08-09 14:37:32 +0200

    update v1 with comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+/* Title: Twitch.tv JSON API v1 (for freeCodeCamp), August 2017
+* Author: Boško Rabrenović
+* https://github.com/boniverski/js-calculator
+* Description: Simple JavaScript Calculator with basci operations.
+*/
+
 $(document).ready(function(){
-  let entries = "";
   let tempEntry = [];
 
-  function defaultStyle(){
+  function defaultStyle(){ // Taking care of default style values on certain clicks
     $(".clr").addClass("hidden");
     $(".del").removeClass("hidden");
     $("#entry").text("0");
@@ -16,37 +21,43 @@ $(document).ready(function(){
     if(!isNaN(val) || val === "." || val === "-"){
       defaultStyle();
 
+      // Adding zero before point symbol if there's no entry
       if((val === ".") && (tempEntry.length === 0)) {
         tempEntry.push(0)
-      } tempEntry.push(val);
+      }
 
+      tempEntry.push(val);
+
+      // Pushing num entries and math signs
     } else if (val === "/" || val === "*" || val === "+" || val==="-"){
         if(tempEntry.length > 0) {
           tempEntry.push(val);
-      } else {
+      } else { // If math sing is entered before number
           tempEntry.push(0);
-          tempEntry.push(val); // If math sing is entered before number
+          tempEntry.push(val);
         }
 
-    } else if (val === "del"){ tempEntry.pop(); }
+    } else if (val === "del"){ tempEntry.pop(); } // Remove last entry
       else if (val === "=") {
         $(".del").addClass("hidden");
         $(".clr").removeClass("hidden");
         $("#tempEntry").addClass("hidden");
         $("#entry").css({"height":"100%", "padding-top":"0.5em"});
 
+        // Checking if last entry is math symbol, before equal sign.
         let lastEntry = tempEntry[tempEntry.length - 1];
         if(lastEntry === "/" || lastEntry === "*" || lastEntry === "+" || lastEntry ==="-") {
-          tempEntry.pop();
+          tempEntry.pop(); // If true, remove it.
         }
 
+        // Covert tempEntry arr before evaulation
         let expression = tempEntry.join("");
         tempEntry = [];
         console.log(`Input entry: ${expression}`);
 
         // Checking if evaluation has error due to odd entries
         try {
-          let result = eval(expression); // Evaluate string with entries
+          let result = eval(expression);
           $("#entry").text(result);
           console.log(`Result: ${result}`);
         } catch (e){
@@ -56,13 +67,14 @@ $(document).ready(function(){
         }
 
     } else if (val === "clr"){
-        // Clearing all entries
-        entries = "";
-        tempEntry = [];
-        console.clear();
-        defaultStyle();
+      defaultStyle();
+      // Clearing all entries
+      entries = "";
+      tempEntry = [];
+      console.clear();
     }
-    //printing final result or default value of "0" if tempEntry is empty array
+
+    // Printing out final result or default value of "0" if tempEntry is empty array
     tempEntry.length > 0 ?
       $("#tempEntry").text(tempEntry.join("")) : $("#tempEntry").text("0");
   });
